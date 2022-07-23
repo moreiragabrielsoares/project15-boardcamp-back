f10418d06672f06e59d4be844d028e28fe73a3be
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-07-23 11:46:02 -0300

    create the route POST /categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -17,9 +17,29 @@ export async function getCategories (req, res) {
 
 export async function postCategories (req, res) {
 
+    const newCategorie = req.body
+
+    const categorieSchema = joi.object({
+        name: joi.string().required()
+    });
+
+    const { error } = categorieSchema.validate(newCategorie);
+    if (error) {
+        res.sendStatus(400);
+        return;
+    }
+
     try {
 
-        res.status(200).send('Test Post Categories');
+        const {rows: categorieDB} = await db.query(`SELECT * FROM categories WHERE name = $1`, [newCategorie.name]);
+        if(categorieDB.length > 0) {
+            res.sendStatus(409);
+            return;
+        }
+
+        await db.query(`INSERT INTO categories (name) VALUES ($1)`, [newCategorie.name]);
+
+        res.sendStatus(201);
 
     } catch (error) {
 
